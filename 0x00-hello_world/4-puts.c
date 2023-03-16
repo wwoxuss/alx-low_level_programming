00c6271ee5f13783cb5fc84d284ec547e758439e
wwoxuss <woxusarts@gmail.com>
2023-03-16 07:56:59 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/*The function main prints a string line*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
