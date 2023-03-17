91071b5ba5b1aedd93d988cd9073d9792e57f4c0
wwoxuss <woxusarts@gmail.com>
2023-03-17 09:38:49 -0700

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints digit numbers from 0 to 10.
+ * Return: returns 0.
+ */
+int main(void)
+{
+	for(int i = 0; i < 10; i++)
+	{
+		printf("%d", n);
+	}
+	printf("\n);
+	return 0;
+}
