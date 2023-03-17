c9d51a35cafe0a9753efbb91be0fe4952a7ef80b
wwoxuss <woxusarts@gmail.com>
2023-03-17 10:44:11 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,7 +16,7 @@ int main(void)
 			{
 				putchar(i);
                         	putchar(j);
-                         	if (i != 57 || j != 56)
+                         	if (i != 57 || j != 57)
                         	{
                                 	putchar(',');
                                 	putchar(' ');
