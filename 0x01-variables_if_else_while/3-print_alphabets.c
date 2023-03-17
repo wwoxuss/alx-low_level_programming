cf9e2bc52e6d2f80d3f3cce134057be462e839b9
wwoxuss <woxusarts@gmail.com>
2023-03-17 09:19:13 -0700

    add

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints lowercase and uppercase alphabets.
+ * Return: returns 0.
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
