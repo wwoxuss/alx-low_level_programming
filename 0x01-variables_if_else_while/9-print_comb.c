d9d17d9764c344d8c606374b0e37f065e67e862e
wwoxuss <woxusarts@gmail.com>
2023-03-17 10:25:12 -0700

    add

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints digit numbers from 0 to 10.
+ * Return: returns 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (putchar != 57)
+		{
+			putchar(", ");
+		}
+	}
+	putchar("\n");
+	return (0);
+}
