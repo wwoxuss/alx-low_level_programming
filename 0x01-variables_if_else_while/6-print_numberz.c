fbcb4e9ed29af1a7dad1a237263839ec704a096d
wwoxuss <woxusarts@gmail.com>
2023-03-17 09:51:01 -0700

    add

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints digit numbers from 0 to 10.
+ * Return: returns 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
