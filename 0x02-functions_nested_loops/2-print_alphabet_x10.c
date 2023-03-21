188e3e6576872062f3731ca0975fe9409274b466
wwoxuss <woxusarts@gmail.com>
2023-03-21 16:57:12 -0700

    add

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,12 @@
  */
 void print_alphabet_x10(void)
 {
-        char ch = 'a';
+        char ch;
 	int i = 0;
 	
 	while (i < 10)
-	{	
+	{
+		ch = 'a';	
         	while (ch <= 'z')
         	{
                 	_putchar(ch);
