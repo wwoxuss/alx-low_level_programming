13f2add5c86e51b8472f02739ecd094b3c3e5ca3
wwoxuss <woxusarts@gmail.com>
2023-03-21 16:59:52 -0700

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
  */
 void print_alphabet_x10(void)
 {
-        char ch;
+	char ch;
 	int i = 0;
-	
+
 	while (i < 10)
 	{
-		ch = 'a';	
-        	while (ch <= 'z')
-        	{
-                	_putchar(ch);
-                	ch++;
-        	}	
-        	_putchar('\n');
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}	
+		_putchar('\n');
 		i++;
 	}
 }
