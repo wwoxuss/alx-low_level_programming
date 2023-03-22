fd16dade4c0bc91adb28dfd1f789037e7c916d7d
wwoxuss <woxusarts@gmail.com>
2023-03-21 17:38:46 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,9 @@
 /**
  * print_sign - Prints the sign of a number.
  * n: The number to be checked.
- * Return: returns 1 if the number is positive, 0 if the number equals 0, -1 if the number is negative.
+ * Return: returns 1 if the number is positive, 
+ * 0 if the number equals 0, 
+ * -1 if the number is negative.
  */
 int print_sign(int n)
 {
