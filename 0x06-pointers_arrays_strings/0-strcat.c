f51e9ac77955bf02c424a68042a7249979be3822
wwoxuss <woxusarts@gmail.com>
2023-03-30 14:38:08 -0700

    add

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,50 @@
 #include "main.h"
+
+int get_length(char *s);
+
 /**
- * _strcat - concatenates 2 strings.
- * @dest: string with concatenation
- * @src: string to be concatenated
- * Return: void.
+ * _strcat - Concatenates the string pointed to by @src,
+ * including the terminating null byte,
+ * to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ * Return: A pointer to the destination string @dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
+	int initial_end;
 	int j;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
+	initial_end = get_length(dest);
 	j = 0;
 	while (src[j] != '\0')
 	{
-		dest[i] = src[j};
-		i++;
+		dest[initial_end + j] = src[j];
 		j++;
 	}
-	dest[i] = '\0';
+	dest[initial_end + j] = '\0';
+
 	return (dest);
+}
+
+/**
+ * get_length - returns the length of a string
+ * @s: String whose length we wish to determine
+ *
+ * Return: Length of s
+ */
+int get_length(char *s)
+{
+	int length;
+	int i;
+
+	length = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		++length;
+		i++;
+	}
+
+	return (length);
 }
