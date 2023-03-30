fc7cef551c5ea987279bf68bbe003adde5e42255
wwoxuss <woxusarts@gmail.com>
2023-03-30 14:24:20 -0700

    add

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,27 @@
-#include main.h"
+#include "main.h"
 /**
  * _strcat - concatenates 2 strings.
  * @dest: string with concatenation
  * @src: string to be concatenated
- * Return: Always 0.
+ * Return: returns dest.
  */
 char *_strcat(char *dest, char *src)
 {
-	int lengthD, lengthS;
+	int i;
+	int j;
 
-	lengthD = 0;
-	lengthS = 0;
-
-	while (*(dest + lengthD) != '\0')
-		lengthD++;
-
-	while (*(src + lengthS) != '\0' && lengthD < 97)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		*(dest + lengthD) = *(src + lengthS);
-		lengthD++;
-		lengthS++;
+		i++;
 	}
-	*(dest + lengthD) = '\0';
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j};
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
